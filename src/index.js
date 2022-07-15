d1d6e2f8b9fa2171341b7f7c54a65ed1707037f2
Tong Chen <1141425427@qq.com>
2022-07-15 15:51:53 +0100

    Basic Render in React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React - 框架核心包
+// ReactDOM - 专门做渲染相关的包
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 应用的全局样式文件
+import './index.css'
+// 引入根组件
+import App from './App'
+
+// 渲染根组件app 到一个id为root的dom节点上
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  // 严格模式节点需要去掉 因为会影响useEffect的执行时机
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
