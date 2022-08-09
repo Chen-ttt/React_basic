ea1aa838137f95e71019a0d6b68f94c37e678f1b
Tong Chen <1141425427@qq.com>
2022-08-09 17:56:04 +0100

    Reducer of Calculator

--- src/Redux/reducers/math.js
+++ src/Redux/reducers/math.js
@@ -2,9 +2,10 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-09 16:33:38
- * @LastEditTime: 2022-08-09 17:52:08
+ * @LastEditTime: 2022-08-09 17:55:50
  * @LastEditors:  
  */
+
 // state默认值为10
 // 每次查看或修改state都会调用reducer函数
 const reducer = (state = 10, action) => {
