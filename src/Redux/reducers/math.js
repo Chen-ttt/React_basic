75662a358d268474b3dc0d97492671a71d050393
Tong Chen <1141425427@qq.com>
2022-08-17 18:01:53 +0100

    Reducer of mockDB

--- src/Redux/reducers/math.js
+++ src/Redux/reducers/math.js
@@ -2,19 +2,33 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-09 16:33:38
- * @LastEditTime: 2022-08-09 17:55:50
+ * @LastEditTime: 2022-08-17 16:23:08
  * @LastEditors:  
  */
 
 // state默认值为10
 // 每次查看或修改state都会调用reducer函数
-const reducer = (state = 10, action) => {
+const reducer = (state = { num: 10 }, action) => {
   console.log("enter")
   switch (action.type) {
     case 'ADD':
-      return state + action.num
+      return {
+        ...state,
+        num: state.num + action.num
+      }
     case 'SQUARE':
-      return state * state
+      return {
+        ...state,
+        num: state.num * state.num
+      }
+
+    case "GET": {
+      console.log("enter GET")
+      return {
+        ...state,
+        data: action.data
+      }
+    }
     default:
       return state
   }
