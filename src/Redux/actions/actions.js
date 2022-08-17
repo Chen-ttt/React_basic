6a9bf01776a4475a90faf3fb1d093cc578d1331f
Tong Chen <1141425427@qq.com>
2022-08-17 18:02:50 +0100

    Actions with Redux-thunk

--- src/Redux/actions/actions.js
+++ src/Redux/actions/actions.js
@@ -2,9 +2,12 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-08-09 16:24:49
- * @LastEditTime: 2022-08-09 16:32:46
+ * @LastEditTime: 2022-08-16 18:36:18
  * @LastEditors:  
  */
+
+import axios from "axios"
+import "../mockDatabase/manufacturerMock"
 
 /**
  * action 可以直接写成一个对象, 也可以写一个返回action对象的函数
@@ -23,16 +26,22 @@ const squareAction = () => {
   }
 }
 
-// const addTwo = {
-//   type: 'ADD',
-//   num: 2
-// }
-
-// const square = {
-//   type: 'SQUARE'
-// }
+// 异步请求 - action creator
+const getAction = () => {
+  return (dispatch) => {
+    axios.get('/manufacturerMock', { dataType: 'json' })
+      .then(res => {
+        console.log("mock data", res.data.manufacturerInfo)
+        dispatch({
+          type: 'GET',
+          data: res.data.manufacturerInfo
+        })
+      })
+  }
+}
 
 export {
   addAction,
-  squareAction
+  squareAction,
+  getAction
 }
